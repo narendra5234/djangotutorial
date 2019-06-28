e42ec6ad61bdeb366d56fd878374ed93b1aa0006
Narendra <you@example.com>
2019-06-28 11:51:57 +0530

    hands one done upto relationships

--- polls/admin.py
+++ polls/admin.py
@@ -2,12 +2,16 @@ from django.contrib import admin
 
 from django.contrib import admin
 
-from .models import Question,Choice
+from .models import Question,Choice,Musician,Album,Person,Fruit
 class QuestionAdmin(admin.ModelAdmin):
     list_display = ('question_text', 'pub_date','was_published_recently')
     list_filter = ['pub_date']
     search_fields = ['question_text']
 admin.site.register(Question,QuestionAdmin)
-admin.site.register(Choice)
+admin.site.register(Musician)
+admin.site.register(Album)
+admin.site.register(Person)
+admin.site.register(Fruit)
+
 
 # Register your models here.
--- polls/models.py
+++ polls/models.py
@@ -21,5 +21,24 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
     def __str__(self):
         return self.choice_text
-# class Fruit(models.Model):
-#     name=models.CharField(max_length=100, primary_key=True)
+class Musician(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    instrument = models.CharField(max_length=100)
+
+class Album(models.Model):
+    artist = models.ForeignKey(Musician, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    release_date = models.DateField()
+    num_stars = models.IntegerField()
+
+class Person(models.Model):
+    SHIRT_SIZES = (
+        ('S', 'Small'),
+        ('M', 'Medium'),
+        ('L', 'Large'),
+    )
+    name = models.CharField(max_length=60)
+    shirt_size = models.CharField(max_length=1, choices=SHIRT_SIZES)
+class Fruit(models.Model):
+    name=models.CharField(max_length=100, primary_key=True)
